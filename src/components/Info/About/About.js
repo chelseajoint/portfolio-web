6bf7aec184a055a219f373f7390d1e41cbc5c3df
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2024-04-06 12:43:16 +0200

    fix mobile styles, add about text

--- src/components/Info/About/About.js
+++ src/components/Info/About/About.js
@@ -7,9 +7,21 @@ const About = () => {
         <section className='about' id='about'>
             <h3 className='title-about'>Quien soy</h3>
             <h4>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco <span className='accent'>laboris</span> nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <span className='accent'>reprehenderit</span> in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.   
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea <span className='accent'>commodo consequat</span>. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat <span className='accent'>cupidatat non proident</span>, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            ¡Hola!
+            <br/><br/>
+Soy Mariia, una apasionada programadora front-end con una sólida experiencia en diseño gráfico. Mi trayectoria académica comenzó con una licenciatura en Diseño de Publicidad en la Universidad Estatal de Tecnología y Diseño de San Petersburgo, donde obtuve mi título en 2015. Posteriormente, continué mi formación con un Máster en Diseño Editorial: Medios Impresos y Digitales en el IED Madrid, del cual me gradué en 2017.
+<br/><br/>
+Empecé mi carrera como diseñadora gráfica pero durante mi trabajo en <span className='accent'>Wunderman Thomson</span> y <span className='accent'>Craft</span> trabajé diseñando y programando las piezas creativas y fue ahí donde descubrí mi pasión por la programación. Durante los últimos años he estado enfocando mi carrera en desarrollo web, aprendiendo lenguajes de programación como <span className='accent'>JavaScript</span> y <span className='accent'>TypeScript</span>, bibliotecas y frameworks como <span className='accent'>React</span>, paquetes de módulos como <span className='accent'>Webpack</span>, <span className='accent'>Parcel</span> y <span className='accent'>Docker</span>, bases de datos, <span className='accent'>API</span>, <span className='accent'>git</span>, <span className='accent'>testing</span> y mucho mas para tener un perfil más avanzado y especializado.
+<br/><br/>
+Actualmente, trabajo como programadora front-end, combinando mi pasión por el diseño con mi habilidad para crear experiencias interactivas y funcionales en la web. 
+<br/><br/>
+He tenido la fortuna de trabajar en diversos proyectos para marcas reconocidas como <span className='accent'>BMW</span>, <span className='accent'>Asisa</span>, <span className='accent'>Nivea</span>, <span className='accent'>Danone</span>, <span className='accent'>Listerine</span>, <span className='accent'>Iberia</span>, <span className='accent'>Swarovski</span>, <span className='accent'>Opel</span> y <span className='accent'>L'Oréal</span>, entre otras.
+<br/><br/>
+Me apasiona crear interfaces intuitivas y atractivas, y tengo experiencia en el desarrollo de aplicaciones web interactivas y responsive.
+<br/><br/>
+Si tienes alguna pregunta o simplemente quieres charlar, no dudes en ponerte en contacto conmigo. Estoy aquí para ayudar en lo que necesites.
+<br/><br/>
+¡Gracias por tu interés y espero tener la oportunidad de colaborar juntos!
             </h4>
         </section>
     )
