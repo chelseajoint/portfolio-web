886313cb67de66139a51973ca553dc01a87bb1b8
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2024-05-06 10:16:45 +0200

    fix mobile styles, add rotate page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Title from './components/Title/Title';
 import Info from './components/Info/Info';
 import Contact from './components/Contact/Contact';
+import RotateDevice from './components/RotateDevice/RotateDevice';
 // import Background from './components/Background/Background';
 
 import './App.scss';
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className="app">
       {/* <Background /> */}
+      <RotateDevice/>
       <div className='content'>
       <Title />
       <Info />
---
+++ src/components/RotateDevice/RotateDevice.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './RotateDevice.scss';
+
+const RotateDevice = () => {
+  return (
+      <div className='rotate-device'>
+        <div className='spin-logo'></div>
+        <h4>Gira el dispositivo</h4>
+      </div>
+  );
+}
+
+export default RotateDevice;
