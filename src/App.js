cb4fbec79542ae231e0e09b822400187b3c10d95
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2024-05-08 11:16:52 +0200

    add boot simulation, add intro animation

--- src/App.js
+++ src/App.js
@@ -9,25 +9,13 @@ import "./App.scss";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
-  const [isContentVisible, setIsContentVisible] = useState(false);
 
   useEffect(() => {
-    fetch('https://mariiakoikonova.com/')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        setIsLoading(false);
-        setIsContentVisible(true);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        setIsLoading(false);
-      });
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timeout);
   }, []);
 
   return (
@@ -37,7 +25,7 @@ function App() {
       ) : (
         <>
           <RotateDevice />
-          <div className={`content ${isContentVisible ? 'visible' : ''}`}>
+          <div className="content">
             <Title />
             <Info />
             <Contact />
