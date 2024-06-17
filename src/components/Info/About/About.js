dfcdda6f02a2e788ff28dff777457af7268e0afa
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2024-06-17 11:21:43 +0200

    fix text, add icon

--- src/components/Info/About/About.js
+++ src/components/Info/About/About.js
@@ -14,20 +14,14 @@ const About = ({ isOpen, toggle }) => {
           <br />
           <br />
           Soy Mariia, una apasionada programadora front-end con una sólida
-          experiencia en diseño gráfico. Mi trayectoria académica comenzó con
-          una licenciatura en Diseño de Publicidad en la Universidad Estatal de
-          Tecnología y Diseño de San Petersburgo, donde obtuve mi título en
-          2015. Posteriormente, continué mi formación con un Máster en Diseño
-          Editorial: Medios Impresos y Digitales en el IED Madrid, del cual me
-          gradué en 2017.
+          experiencia en diseño gráfico.
           <br />
           <br />
           Empecé mi carrera como diseñadora gráfica pero durante mi trabajo en{" "}
-          <span className="accent">Wunderman Thomson</span> y{" "}
-          <span className="accent">Craft</span> trabajé diseñando y programando
-          las piezas creativas y fue ahí donde descubrí mi pasión por la
-          programación. Durante los últimos años he estado enfocando mi carrera
-          en desarrollo web, aprendiendo lenguajes de programación como{" "}
+          <span className="accent">S,C,P,F...</span> y trabajé diseñando y
+          programando las piezas creativas y fue ahí donde descubrí mi pasión
+          por la programación. Durante los últimos años he estado enfocando mi
+          carrera en desarrollo web, aprendiendo lenguajes de programación como{" "}
           <span className="accent">JavaScript</span> y{" "}
           <span className="accent">TypeScript</span>, bibliotecas y frameworks
           como <span className="accent">React</span>, paquetes de módulos como{" "}
@@ -60,15 +54,6 @@ const About = ({ isOpen, toggle }) => {
           Me apasiona crear interfaces intuitivas y atractivas, y tengo
           experiencia en el desarrollo de aplicaciones web interactivas y
           responsive.
-          <br />
-          <br />
-          Si tienes alguna pregunta o simplemente quieres charlar, no dudes en
-          ponerte en contacto conmigo. Estoy aquí para ayudar en lo que
-          necesites.
-          <br />
-          <br />
-          ¡Gracias por tu interés y espero tener la oportunidad de colaborar
-          juntos!
         </h4>
       </div>
     </section>
--- src/components/Info/Projects/Projects.js
+++ src/components/Info/Projects/Projects.js
@@ -16,28 +16,28 @@ const Projects = ({ isOpen, toggle }) => {
     <section className='projects' id='projects'>
       <h3 className={`title-projects ${isOpen ? 'active' : ''}`} onClick={toggle}>Proyectos</h3>
       <div className={`mobile-dropdown ${isOpen ? 'open' : ''}`}>
-      {projectsData.map((project, index) => (
-        <button
-          className='project-info'
-          key={index}
-          href={project.link}
-          onClick={(e) => {
-            e.preventDefault();
-            handleProjectClick(project.link);
-          }}
-        >
+        {projectsData.map((project, index) => (
+          <button
+            className='project-info'
+            key={index}
+            href={project.link}
+            onClick={(e) => {
+              e.preventDefault();
+              handleProjectClick(project.link);
+            }}
+          >
             <h3 className='name'>{project.name}</h3>
             <div className='about-project'>
-            <h4>{project.about}</h4>
-            <div className='skills'>
-              {project.skills.map((skill, idx) => (
-                <h4 key={idx}>{skill}</h4>
-              ))}
+              <h4 dangerouslySetInnerHTML={{ __html: project.about }}></h4>
+              <div className='skills'>
+                {project.skills.map((skill, idx) => (
+                  <h4 key={idx}>{skill}</h4>
+                ))}
+              </div>
             </div>
-          </div>
-        </button>
-      ))}
-      <button className='view-all' onClick={handleViewAll}><h4>Ver github</h4></button>
+          </button>
+        ))}
+        <button className='view-all' onClick={handleViewAll}><h4>Ver github</h4></button>
       </div>
     </section>
   );
